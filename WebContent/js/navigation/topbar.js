6a343b1e64b47c22515818c3f168da3c4e7417e1
husen <husen@hemingsheng.cn>
2017-12-01 09:58:19 +0800

    搜索框绑定回车键

--- WebContent/js/navigation/topbar.js
+++ WebContent/js/navigation/topbar.js
@@ -6,45 +6,49 @@
  * 2017年10月18日
  */
 
-$(document).ready(function(){
-	//选择的栏目增加active
-	var menuBarNo = Number($('#menuBarNo').val());
-	$('.nav.nav-pills.topbar-nav').children().removeClass('active').eq(menuBarNo).addClass('active');
-	
-	//加载当前网站访问统计
-	loadAccessStatistics();
-	
-	//搜索类型选择事件注册
-	chooseSearchType();
-});
+$(document).ready(
+		function() {
+			// 选择的栏目增加active
+			var menuBarNo = Number($('#menuBarNo').val());
+			$('.nav.nav-pills.topbar-nav').children().removeClass('active').eq(
+					menuBarNo).addClass('active');
+
+			// 加载当前网站访问统计
+			loadAccessStatistics();
+
+			// 搜索类型选择事件注册
+			chooseSearchType();
+			//绑定回车键
+			searchKeyEvent();
+		});
 
 /**
  * 加载当前网站访问统计
  * 
  * @returns
  */
-function loadAccessStatistics(){
+function loadAccessStatistics() {
 	$.ajax({
 		type : 'POST',
 		url : '/accessAtatistics.hms',
 		dataType : 'json',
-		success : function(response, ststus){
+		success : function(response, ststus) {
 			$('#txt_accessToday').html('今日访问量：' + response.accessToday + ',');
 			$('#txt_accessTotal').html('总访问量：' + response.accessTotal + ',');
 			$('#txt_onlineCurrent').html('当前在线人数：' + response.onlineCurrent);
 		},
-		error : function(XMLHttpRequest, textStatus){
+		error : function(XMLHttpRequest, textStatus) {
 			$.confirm({
-			    title: '网站访问统计加载出错',
-			    content: textStatus + ' : ' + XMLHttpRequest.status,
-			    autoClose: 'ok|2000',
-			    type: 'red',
-			    buttons: {
-			    	ok: {
-			            text: '确认',
-			            btnClass: 'btn-primary',
-			        },
-			    }
+				title : '网站访问统计加载出错',
+				content : textStatus + ' : ' + XMLHttpRequest.status,
+				autoClose : 'ok|2000',
+				type : 'red',
+				buttons : {
+					ok : {
+						text : '确认',
+						btnClass : 'btn-primary',
+					},
+				}
 			});
 		}
 	});
@@ -55,45 +59,63 @@ function loadAccessStatistics(){
  * 
  * @returns
  */
-function nextThemeClick(){
+function nextThemeClick() {
 	var curr_theme_no = 0;
-	if(typeof $.cookie('theme_no') == 'undefined'){
+	if (typeof $.cookie('theme_no') == 'undefined') {
 		$.cookie('theme_no', 0);
-	}else{
+	} else {
 		curr_theme_no = Number($.cookie('theme_no'));
-		$.cookie('theme_no', curr_theme_no+1);
+		$.cookie('theme_no', curr_theme_no + 1);
 	}
 
-	curr_theme_no = (curr_theme_no+1)%5;
-	
+	curr_theme_no = (curr_theme_no + 1) % 5;
+
 	var img = '/images/background/bg-' + curr_theme_no + '.jpg';
-	$('body').css('background-image','url(' + img + ')');
+	$('body').css('background-image', 'url(' + img + ')');
 }
 
 /**
  * 选择搜索类型
+ * 
  * @returns
  */
-function chooseSearchType(){
-	$('#choose_search_type').find('.dropdown-menu').children('li').click(function() {
-		searchByKeyWords($(this).attr('value'));
-	});
-	
-	$('#search_blog').click(function(){
+function chooseSearchType() {
+	$('#choose_search_type').find('.dropdown-menu').children('li').click(
+			function() {
+				searchByKeyWords($(this).attr('value'));
+			});
+
+	$('#search_blog').click(function() {
 		searchByKeyWords('blog');
 	})
 }
 
 /**
  * 跳转到相应页面，并附带搜索参数
+ * 
  * @param searchType
  * @returns
  */
-function searchByKeyWords(searchType){
-	if(searchType == 'blog'){
-		$(window).attr('location','/topic/blog/?keywords='+$('#search_keyWords').val());
+function searchByKeyWords(searchType) {
+	if (searchType == 'blog') {
+		$(window).attr('location',
+				'/topic/blog/?keywords=' + $('#search_keyWords').val());
 	}
-	if(searchType == 'code'){
-		$(window).attr('location','/topic/code/?keywords='+$('#search_keyWords').val());
+	if (searchType == 'code') {
+		$(window).attr('location',
+				'/topic/code/?keywords=' + $('#search_keyWords').val());
 	}
+}
+
+/**
+ * 搜索输入框绑定回车键
+ * 
+ * @returns
+ */
+function searchKeyEvent() {
+	$('#search_keyWords').keypress(function(event) {
+		if (event.keyCode == 13) {
+			searchByKeyWords('blog');
+		}
+	});
 }
