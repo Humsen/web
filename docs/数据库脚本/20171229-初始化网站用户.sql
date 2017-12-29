45ca15cf1cfb031d130f74c7c05163938502c067
Husen <husen@hemingsheng.cn>
2017-12-29 16:23:07 +0800

    增加脚本

--- docs/数据库脚本/20171229-初始化网站用户.sql
+++ docs/数据库脚本/20171229-初始化网站用户.sql
@@ -9,3 +9,6 @@ INSERT INTO user_info (user_id, user_name, user_password, user_email) VALUES
 (0, 'husen', '7c5c8c3ac9fda2953de063d1f069c7f7', 'husen@hemingsheng.cn'),
 (1, 'super_admin', '2f0b3214c90231b9ad9f341bf1df8035', 'he_mingsheng@qq.com'),
 (2, 'admin', 'c19380b712c30d00881bc9cfb3c74050', '940706904@qq.com');
+
+-- 插入husen用户昵称
+UPDATE user_info SET user_nick_name = '何明胜' WHERE user_id = 0;
